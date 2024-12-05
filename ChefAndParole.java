136ad86d1503af52f626d35f19af8dd8bfbca973
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 00:28:31 +0530

    Add files via upload

---
+++ ChefAndParole.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefAndParole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+            if(X>=7)
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("no");
+             }
+        s.close();
+    }
+}
