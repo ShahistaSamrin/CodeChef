7026ca6b40c34328ec126bb2318c88b01a4102f1
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 00:30:34 +0530

    Add files via upload

---
+++ PuzzleHunt.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PuzzleHunt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		if(N>=6 && N<=8)
+		{
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+    }
+}
