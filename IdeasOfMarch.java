20acdc544969b58251a6d96989696f966e6b7f94
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 00:29:46 +0530

    Add files via upload

---
+++ IdeasOfMarch.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class IdeasOfMarch 
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        if (N == 15) 
+        {
+            System.out.println("Yes");
+        } 
+        else 
+        {
+            System.out.println("No");
+        }
+        s.close();
+    }
+}
