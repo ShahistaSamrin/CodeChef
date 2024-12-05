626ea2dc066a2f0bb53bc7508ae0656ffc7a8919
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 00:30:52 +0530

    Add files via upload

---
+++ Sportssection.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Sportssection
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>=8 && X<=10)
+		{
+		    System.out.println("Yes");
+		}
+		else
+		{
+		    System.out.println("No");
+		}
+
+	}
+}
