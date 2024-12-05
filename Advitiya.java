ba2d63cd068ff907ae5c00ac5480fdf32bd91c42
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 00:28:05 +0530

    Add files via upload

---
+++ Advitiya.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Advitiya
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n==16||n==17||n==18)
+		{
+		    System.out.println("Advitiya");
+		}
+		else
+		{
+		    System.out.println("Not Advitiya");
+		    
+		}
+		
+
+	}
+}
