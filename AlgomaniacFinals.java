25387ec070414b83854e533f9165e63b664ea6a5
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-09 21:26:04 +0530

    Add files via upload

---
+++ AlgomaniacFinals.java
@@ -0,0 +1,16 @@
+/*  17
+    YAY  */
+import java.util.*;
+class AlgomaniacFinals{
+    public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X==17)
+		{
+		    System.out.println("YAY");
+		}else{
+		    System.out.println("NO");
+		}
+	}
+}
