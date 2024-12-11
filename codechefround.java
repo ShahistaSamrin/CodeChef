f8f90261fc5ab4416243b66de249371df5724d46
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 22:04:19 +0530

    Add files via upload

---
+++ codechefround.java
@@ -0,0 +1,15 @@
+/*  1//input
+    NO//output  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N==4)
+        {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
