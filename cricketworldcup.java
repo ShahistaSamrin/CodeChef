fcd3cea2d3a9e1e1fc1f6df8c0976d5d927f4ba6
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 22:05:31 +0530

    Add files via upload

---
+++ cricketworldcup.java
@@ -0,0 +1,16 @@
+/*  3//input
+    NO//output   */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner (System.in);
+	int X=sc.nextInt();
+	if(X>=12)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+
+	}
+}
