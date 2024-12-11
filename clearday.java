f74d89c2c24e86198225f87d554207cc819010a8
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 22:04:59 +0530

    Add files via upload

---
+++ clearday.java
@@ -0,0 +1,15 @@
+/* 2 3//input
+   2//output  */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner	n=new Scanner (System.in);
+	int X=n.nextInt();
+	int Y=n.nextInt();
+	int z=7-(X+Y);
+	System.out.println(z);
+
+	}
+}
