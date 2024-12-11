bf658e38b88590fe8cd9bcbd6ba06465cf296ad3
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 22:05:48 +0530

    Add files via upload

---
+++ Diwalidiscount.java
@@ -0,0 +1,12 @@
+/*  3000 1000
+2000 //ouput  */
+import java.util.*;
+class codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        int Z=Math.max(0,A-B);
+        System.out.println(Z);
+    }
+}
