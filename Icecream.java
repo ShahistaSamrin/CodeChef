b805647ba7c72f1ad0b99206d4638c4bab2f1ca7
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-13 19:48:42 +0530

    Add files via upload

---
+++ Icecream.java
@@ -0,0 +1,18 @@
+/* 5 10//input
+   YES //output  */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner (System.in);
+	int X=sc.nextInt();
+	int Y=sc.nextInt();
+	int z=2*X;
+	if(Y>=z)
+	System.out.println("YES");
+	else 
+	System.out.println("No");
+
+	}
+}
