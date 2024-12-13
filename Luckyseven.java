3befd7a2f2a96cc24a059a644ff0621c0ec06577
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-13 19:49:01 +0530

    Add files via upload

---
+++ Luckyseven.java
@@ -0,0 +1,15 @@
+/*  proceeding//input 
+    d//output          */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class codechef
+{
+    public  static void main(String args[]) throws java.lang.Exception
+    {
+    Scanner sc = new Scanner(System.in);
+    String S=sc.next();
+    System.out.println(S.charAt(6));
+    }
+} 
