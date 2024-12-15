23f0350b3804d67506f909580a27ba308fa57010
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-15 20:17:32 +0530

    Add files via upload

---
+++ Allnewcodechef.java
@@ -0,0 +1,19 @@
+/*  255 230//input  New//output   */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		if(X<Y){
+		    System.out.println("Old");
+		}
+		else if(X==Y){
+		    System.out.println("Same");
+		}else{
+		    System.out.println("New");
+		}
+	}
+}
