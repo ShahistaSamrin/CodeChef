e3943482fa932d65df7664709f0b508725ef47fb
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-15 20:18:41 +0530

    Add files via upload

---
+++ ValentinesContest.java
@@ -0,0 +1,20 @@
+/*  121 //input     Likely//output  */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		if(N>=120 && N<=123)
+		{
+		    if(N==121)
+		    {
+		        System.out.println("Likely");
+		    }
+		    else{
+		     System.out.println("unlikely");
+		    }
+		}
+	}
+}
