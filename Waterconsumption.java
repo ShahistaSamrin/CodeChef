dacf0195547d1e968aab0df0e37a4cc06eb04b1f
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-15 20:18:57 +0530

    Add files via upload

---
+++ Waterconsumption.java
@@ -0,0 +1,25 @@
+/* 3//input values  
+2999//input     YES
+1450            NO
+2000            YES  */
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i=1;i<=T;i++)
+		{
+	    int X=sc.nextInt();
+	    if(X>=2000)
+	    {
+	        System.out.println("YES");
+	    }
+	    else{
+	        System.out.println("NO");
+	    }
+	    
+		}	
+	}
+}
