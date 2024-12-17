f77920e7d5ae51c39e73f98f410b9936ab0dd2f7
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-17 21:46:43 +0530

    Add files via upload

---
+++ 404notfound.java
@@ -0,0 +1,18 @@
+/*  200//input   FOUND//output  */
+import java.util.*;
+class 404notfound
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	if(X==404)
+	{
+	    System.out.println("NOT FOUND");
+	}
+	else{
+	    System.out.println("FOUND");
+	}
+
+	}
+}
