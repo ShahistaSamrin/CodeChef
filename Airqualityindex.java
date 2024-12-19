efd926c3b9f87b0d4022069abfbfe64353c9685b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-19 21:59:59 +0530

    Add files via upload

---
+++ Airqualityindex.java
@@ -0,0 +1,18 @@
+/*  50//input   YES//output   */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new  Scanner(System.in);
+		int x=sc.nextInt();
+		if(x<100)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
