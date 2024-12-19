fe61787313560253f2dae68e5b38baad20d5d30e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-19 22:01:54 +0530

    Add files via upload

---
+++ Whomakesp1.java
@@ -0,0 +1,19 @@
+/*  5 5 //input      Tyro //output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	if(a<=b)
+	System.out.println("Tyro");
+	else
+	System.out.println("Dom");
+
+	}
+}
