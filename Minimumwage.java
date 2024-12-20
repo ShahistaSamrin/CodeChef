fdf46f48ea89e6b6522978aa32d842b948abba90
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-20 21:48:44 +0530

    Add files via upload

---
+++ Minimumwage.java
@@ -0,0 +1,16 @@
+/* 20/input   YES//Output  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        if(t>11)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
