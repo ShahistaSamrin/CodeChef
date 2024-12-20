e97f3fc3545b3dee879a306ae11d0a46023f7e7e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-20 21:46:57 +0530

    Add files via upload

---
+++ Foodcosts.java
@@ -0,0 +1,14 @@
+/* 100 500//input  1100//output  */
+import java.util.*;
+class Foodcosts{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int d=a*6+b;
+        System.out.println(d);
+        
+       
+    }
+}
