718b9af517204e44ec01c994dca5b4e00c0378ed
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-20 21:49:22 +0530

    Add files via upload

---
+++ Numbermirror.java
@@ -0,0 +1,10 @@
+/* 123//input  123//output   */
+import java.util.*;
+class Codechef{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        System.out.println(t);
+    }
+}
