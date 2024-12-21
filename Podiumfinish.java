d4e1db0cacbcd71de62a0876acdd742dd5ec42fb
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-21 22:23:09 +0530

    Add files via upload

---
+++ Podiumfinish.java
@@ -0,0 +1,17 @@
+/* 4
+1 1//input    2
+2 5           7
+3 2           5
+5 6           11  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int A=s.nextInt();
+            int B=s.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
