6fbfb8df0001073677bb626e351b91fe84ab359f
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-21 22:21:16 +0530

    Add files via upload

---
+++ Boneappetit.java
@@ -0,0 +1,13 @@
+/* 4 2//input   32//output
+   5 6                    */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        int M=s.nextInt();
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        System.out.println((N*X)+(M*Y));
+    }
+}
