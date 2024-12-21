04c9123260bf2864e9a2a7fd52a1222ffa8db9fa
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-21 22:21:36 +0530

    Add files via upload

---
+++ Christmascake.java
@@ -0,0 +1,9 @@
+/*  18//input    7//output  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        System.out.println(25-X);
+    }
+}
