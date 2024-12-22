6a7fbb2cafe4a8c944caa330267ce3da6852505b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-22 22:30:51 +0530

    Add files via upload

---
+++ Heightofrationals.java
@@ -0,0 +1,19 @@
+/* 3 19//input  19//output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a<b){
+		    System.out.println(b);
+		}else{
+		    System.out.println(a);
+		}
+	}
+}
