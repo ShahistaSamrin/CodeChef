938fc6e3cd1f7cd32c1d8b92f24438922ec4156b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-23 19:36:18 +0530

    Add files via upload

---
+++ Lunchtime.java
@@ -0,0 +1,26 @@
+/*  3
+1//input  yes//output
+7         no
+3         yes  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		for(int i=0;i<b;i++)
+		{
+		    int a=sc.nextInt();
+		    if(a>=1 && a<=4)
+	     	{
+		        System.out.println("yes");
+	    	}else{
+		        System.out.println("no");
+	    	}
+	    }
+	}
+}
