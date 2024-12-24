a6a0c5514f2b4c77002799482f94af9e200b5c00
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-24 21:43:54 +0530

    Add files via upload

---
+++ FootballTraining.java
@@ -0,0 +1,20 @@
+/*  10 7//input  FREEKICK//output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a>b)
+		{
+		    System.out.println("FREEKICK");
+		}else{
+		    System.out.println("PENALTY");
+		}
+	}
+}
