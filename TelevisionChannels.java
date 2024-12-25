67598e1449f6780595e52338fd6c2373bbdedfd6
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-25 22:51:57 +0530

    Add files via upload

---
+++ TelevisionChannels.java
@@ -0,0 +1,20 @@
+/*  5//input     3//output    */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+	    if(a%2==0)
+	    {
+	        System.out.println(a/2);
+	    }else{
+	        int b=(a/2)+1;
+	        System.out.println(b);
+	    }
+	}
+}
