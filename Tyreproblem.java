8c1ed94376b89618029b96e8ae58e3dc4a6bfc0b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-25 22:53:48 +0530

    Add files via upload

---
+++ Tyreproblem.java
@@ -0,0 +1,22 @@
+/* 2
+2 1//input   8//output
+3 0          6         */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    System.out.println(2*b+4*c);
+		}
+	}
+}
