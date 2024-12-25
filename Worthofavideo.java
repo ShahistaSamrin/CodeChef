48893d9cec8c3e3612615d3888b85fb89f636046
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-25 22:52:18 +0530

    Add files via upload

---
+++ Worthofavideo.java
@@ -0,0 +1,21 @@
+/*  2
+1//input   24000//output
+3          72000       */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner (System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+		    int b=sc.nextInt();
+		    System.out.println(24*1000*b);
+		}
+	}
+}
