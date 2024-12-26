3aecc94634619b2f563bd998111786f671953de7
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-26 20:08:55 +0530

    Add files via upload

---
+++ TrueandFalsePaper.java
@@ -0,0 +1,21 @@
+/*  3
+1 1//input     0//output
+50 0           50
+100 76         24  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++){
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    System.out.println(b-c);
+		}
+	}
+}
