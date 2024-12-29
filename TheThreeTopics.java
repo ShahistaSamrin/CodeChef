5fa1405e52c6165f55971b8dbab1ca4962aa238c
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-29 22:28:15 +0530

    Add files via upload

---
+++ TheThreeTopics.java
@@ -0,0 +1,21 @@
+/*  2 3 7 3//input     yes//output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		if(a==d || b==d || c==d){
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+	}
+}
