f61aa09d2d646f24d629d4507f0d6f82163b910d
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-30 11:58:15 +0530

    Add files via upload

---
+++ 2000.java
@@ -0,0 +1,9 @@
+/*  4//input           16//output       */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.println(2000/500*N);
+    }
+}
