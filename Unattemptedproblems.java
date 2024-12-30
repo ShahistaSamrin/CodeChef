12e3439c59dd7162d191ba62a6734bd279a7da66
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-30 12:00:23 +0530

    Add files via upload

---
+++ Unattemptedproblems.java
@@ -0,0 +1,10 @@
+/*  10 4//input     6//output   */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        System.out.println(X-Y);
+    }
+}
