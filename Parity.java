bb1d28d818def5cef626db2ae89b25307054698e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-31 23:22:22 +0530

    Add files via upload

---
+++ Parity.java
@@ -0,0 +1,20 @@
+/* 4
+10//input    YES //output
+4            YES 
+3            NO
+2            YES  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            if((N&1)==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
