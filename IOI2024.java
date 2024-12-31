e687ee262912744b151f9096e42998dee9919155
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-31 23:22:02 +0530

    Add files via upload

---
+++ IOI2024.java
@@ -0,0 +1,13 @@
+/* 8//input        YES//output  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        if(X<=8){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    } 
+}
