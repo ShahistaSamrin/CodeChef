2af7538c28f39403143a32e593934c008311415f
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-03 22:17:39 +0530

    Add files via upload

---
+++ HelpingChef.java
@@ -0,0 +1,22 @@
+/*  
+1//input               Thanks for helping Chef!//output
+12                     -1
+-5                     Thanks for helping Chef!  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int T=s.nextInt();
+	    for(int i=0;i<T;i++){
+	        int N=s.nextInt();
+	        if(N<10) System.out.println("Thanks for helping Chef!");
+	        else System.out.println("-1");
+	    }
+
+	}
+}
