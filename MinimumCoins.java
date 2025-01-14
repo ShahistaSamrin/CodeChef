4a6a657ffe7c735b396d3ddc281e2569d8c2581f
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-14 20:58:41 +0530

    Add files via upload

---
+++ MinimumCoins.java
@@ -0,0 +1,16 @@
+/*  4
+53/input     3/output
+100          0
+9            9
+11           1   */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            System.out.println(X%10);
+        }
+    }
+}
