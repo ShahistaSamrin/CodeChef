ee33d5e965849b0f5b885a30aa4655392c7995be
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-18 19:50:01 +0530

    Add files via upload

---
+++ ManaPoints.java
@@ -0,0 +1,22 @@
+/* 3
+10 30/input       3/output
+6 41              6
+50 2              0      */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            X=Y/X;
+            System.out.println(X);
+        }
+	}
+}
