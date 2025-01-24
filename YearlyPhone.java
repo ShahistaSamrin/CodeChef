b169ec62bd66417be114314601e6f90b3e3475b8
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-24 20:44:07 +0530

    Add files via upload

---
+++ YearlyPhone.java
@@ -0,0 +1,15 @@
+/*  2000/input         K00/output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        X=X%100;
+        System.out.printf("K%02d\n",X%100);
+	}
+}
