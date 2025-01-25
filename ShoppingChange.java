9acd02a879f19f1e76782cbda3dc297c0f5bfbd0
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-25 18:34:32 +0530

    Add files via upload

---
+++ ShoppingChange.java
@@ -0,0 +1,20 @@
+/* 3
+1 /input         99/output
+25               75
+100              0  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            System.out.println(100-X);
+        }
+	}
+}
