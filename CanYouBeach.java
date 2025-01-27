7e7806509ba066e54a641e09c8cad680742615d6
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-01-27 18:51:27 +0530

    Add files via upload

---
+++ CanYouBeach.java
@@ -0,0 +1,15 @@
+/* 10/input   30/output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        System.out.println(X+2*10);
+
+	}
+}
