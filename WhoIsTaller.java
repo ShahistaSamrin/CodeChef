7ba631dc6a36c0abb005588586cc2d9bc20cf3b2
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-02-02 21:22:45 +0530

    Add files via upload

---
+++ WhoIsTaller.java
@@ -0,0 +1,19 @@
+/* 2
+150 160/input  B/output 
+160 150        A  */
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int A=s.nextInt();
+            int B=s.nextInt();
+            if(A>B){
+                System.out.println("A");
+            }else{
+                System.out.println("B");
+            }
+        }
+    }
+}
