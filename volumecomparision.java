e17652b571d00c81cf31254661030c3adf19545e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-02-04 22:22:12 +0530

    Add files via upload

---
+++ volumecomparision.java
@@ -0,0 +1,25 @@
+/* 1 3 10 3/input  cubiod/output  */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        int C=s.nextInt();
+        int X=s.nextInt();
+        A=A*B*C;
+        X=X*X*X;
+        if(A>X){
+            System.out.println("Cuboid");
+        }else if(A<X){
+            System.out.println("Cube");
+        }else{
+            System.out.println("Equal");
+        }
+	}
+}
