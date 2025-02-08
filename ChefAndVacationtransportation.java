4b4ae0075e47ceb51dffa300746aac2cd16f134b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-02-08 22:41:34 +0530

    Add files via upload

---
+++ ChefAndVacationtransportation.java
@@ -0,0 +1,24 @@
+/* 3
+10 12 11/input   Train/output
+3 5 15           PlaneBus
+9 4 13           Equal   */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=s.nextInt();
+		    int Y=s.nextInt();
+		    int Z=s.nextInt();
+		    if((X+Y)<Z) System.out.println("PlaneBus");
+		    else if((X+Y)==Z) System.out.println("Equal");
+		    else System.out.println("Train");
+		}
+	}
+}
